860fa876db859d884cd916f226bfc6dc8c6104a1
Mohamed Sultan <mohamedsultan@Administrators-MacBook-Pro.local>
2016-11-30 12:13:43 +0000

    Added Conditional

--- src/Conditional.scala
+++ src/Conditional.scala
@@ -1,6 +1,31 @@
 /**
   * Created by mohamedsultan on 30/11/2016.
   */
-class Conditional {
+object Conditional {
+  def main(args: Array[String]): Unit = {
+    // if statements are like Java except they return a value like the
+    // ternary operator
 
+    // Conditional Operators : ==, !=, >, <, >=, <=
+    // Logical Operators : &&, ||, !
+
+    var age = 18
+
+    val canVote = if (age >= 18) "yes" else "no"
+    println(canVote)
+    // You have to use { } in the REPL, but not otherwise
+    if ((age >= 5) && (age <= 6)) {
+      println("Go to Kindergarten")
+    } else if ((age > 6) && (age <= 7)) {
+      println("Go to Grade 1")
+    } else {
+      println("Go to Grade " + (age - 5))
+    }
+    /*
+    true || false
+    !(true)
+
+    */
+
+  }
 }
