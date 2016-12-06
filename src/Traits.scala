0542f1e2e6f67806719ddef70e1f8550c21ae64d
Mohamed Sultan <mohamedsultan@Administrators-MacBook-Pro.local>
2016-12-06 14:27:21 +0000

    Added Traits

--- src/Traits.scala
+++ src/Traits.scala
@@ -2,5 +2,33 @@
   * Created by mohamedsultan on 05/12/2016.
   */
 object Traits {
+  def main(args: Array[String]): Unit = {
+    val superman = new Superhero("Superman")
+    println(superman.fly)
+    println(superman.hitByBullet)
+    println(superman.ricochet(2500))
+  }
 
+  // A trait is a like a Java interface in that a class can extend more then 1
+  // Unlike Java interfaces traits can provide concrete methods and fields
+  trait Flyable {
+    def fly : String
+
+  }
+
+  trait Bulletproof {
+    def hitByBullet : String
+
+    // You can define concrete methods in traits
+    def ricochet(startSpeed : Double) : String = {
+      "The bullet ricochets at a speed of %.1f ft/sec".format(startSpeed * .75)
+    }
+  }
+
+  // The first trait starts with extends and then with for each other
+  class Superhero(val name : String) extends Flyable with Bulletproof{
+    def fly = "%s flys through the air".format(this.name)
+
+    def hitByBullet = "The bullet bounces off of %s".format(this.name)
+  }
 }
